57e3fed2b181567a7f24b8bd41fb1f325350d4e8
shojol021 <shojol021@gmail.com>
2023-06-12 00:12:52 +0600

    make approved api created

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const jwt = require('jsonwebtoken')
 const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const cors = require('cors')
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
 
-const verifyJwt = (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({ error: true, message: 'unauthorized access' })
-  }
-  const token = authorization.split(' ').[1]
-  jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
-    if(error){
-      return res.status(401).send({ error: true, message: 'unauthorized access' })
-    }
-  })
-}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6c8obk5.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -37,58 +24,106 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const instructorCollection = client.db('school').collection('instructor')
     const classesCollection = client.db('school').collection('classes')
     const userCollection = client.db('school').collection('user')
     const selectCollection = client.db('school').collection('selected')
 
-    app.post('jwt', (req, res) => {
+    app.get('/instructors', async (req, res) => {
+      const result = await instructorCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/classes', async (req, res) => {
+      const result = await classesCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/select', async (req, res) => {
+      const cls = req.body;
+      const result = await selectCollection.insertOne(cls)
+      res.send(result)
+    })
+
+    app.get('/select', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await selectCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.get('/users', async (req, res) => {
+      const email = req.query.email
+      const query = { email: email }
+      const result = await userCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/allclasses', async(req, res) => {
+      const result = await classesCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/users', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
-      res.send(token)
+      const result = await userCollection.insertOne(user)
+      res.send(result)
     })
 
-    app.get('/instructors', async(req, res) => {
-        const result = await instructorCollection.find().toArray()
-        res.send(result)
+    app.post('/class', async(req, res) => {
+      const cls = req.body;
+      const result = await classesCollection.insertOne(cls)
+      res.send(result)
     })
 
-    app.get('/classes', async(req, res) => {
-        const result = await classesCollection.find().toArray()
-        res.send(result)
+    app.patch('/user', async (req, res) => {
+      const { email } = req.body;
+      console.log(email)
+      const query = { email: email }
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(query, updatedDoc)
+      res.send(result)
     })
 
-    app.post('/select', async(req, res) => {
-        const cls = req.body;
-        const result = await selectCollection.insertOne(cls)
-        res.send(result)
+    app.patch('/make-approved/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          status: 'approved'
+        }
+      }
+      const result = await classesCollection.updateOne(filter, updatedDoc)
+      res.send(result)
     })
 
-    app.get('/select', async(req, res) => {
-        const email = req.query.email;
-        const query = {email: email}
-        const result = await selectCollection.find(query).toArray(  )
-        res.send(result)
+    app.patch('/make-instructor', async(req, res) => {
+      const { email } = req.body;
+      console.log(email)
+      const query = { email: email }
+      const updatedDoc = {
+        $set: {
+          role: 'instructor'
+        }
+      }
+      const result = await userCollection.updateOne(query, updatedDoc)
+      res.send(result)
     })
 
-    app.get('/users', async(req, res) => {
-        const email = req.query.email
-        const query = {email: email}
-        const result = await userCollection.findOne(query)
-        res.send(result)
+    app.get('/allusers', async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
     })
 
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        const result = await userCollection.insertOne(user)
-        res.send(result)
-    })
-
-    app.post('/payment-intent', async(req, res) => {
-      const {price} = req.body;
-      const amount = price*100;
+    app.post('/payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
       console.log(price, amount)
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
@@ -96,7 +131,7 @@ async function run() {
         payment_method_types: ['card']
       })
       res.send(
-        {clientSecret: paymentIntent.client_secret}
+        { clientSecret: paymentIntent.client_secret }
       )
     })
 
@@ -112,7 +147,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('server is running')
+  res.send('server is running')
 }),
 
-app.listen(port)
+  app.listen(port)
